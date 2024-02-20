2036e8ae48c8e590356bbfe27cf6040bb375c508
Hans Han <hans.han@kellogg.northwestern.edu>
2024-02-20 00:51:09 +0000

    HW3 Pt.1

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,33 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = ["Nice","Beijing", "London"]
-    render :template => "places/index"
+    @places = Place.all
+    #render :template => "places/index"
+  end
 
+  def show
+    @place = Place.find_by({"id" => params["id"]})
+  end
+
+  
+  def new
+    @place = Place.new
+  end
+
+  def create
+
+    @place = Place.new
+
+    # assign user-entered form data to Post's columns
+    @place["name"] = params["name"]
+    
+
+    # save place row
+    @place.save
+
+    # redirect user
+    redirect_to "/places"
+  
   end
 
 
